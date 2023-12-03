76e0538b68b0f85b39e2c18d0710bc65d904e148
aneena ansar <131254828+aneenaansar@users.noreply.github.com>
2023-12-03 12:01:47 +0530

    updated

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from .forms import LoginForm
 
 # Create your views here.
 def login(request):
-    return render(request,'dashboard/login.html')
+    form = LoginForm()
+    return render(request,'dashboard/login.html', {'form': form})
 
 def index(request):
     return render(request,'dashboard/index.html')
 
 def addblog(request):
-    return render(request,'dashboard/addblog')
+    return render(request, 'dashboard/addblog.html')
 
 def detail(request):
     return render(request,'dashboard/detail.html')
