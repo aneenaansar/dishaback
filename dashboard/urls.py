03b3a3932a6117cb6f1d817eee8df6996123d5d0
aneena ansar <131254828+aneenaansar@users.noreply.github.com>
2023-12-03 06:55:16 +0530

    updated

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import superuser_login
 
 from . import views
 
@@ -14,5 +13,4 @@ urlpatterns = [
     path('appoinments/',views.appoinments, name='appoinments'),
     path('displayblog/',views.displayblog, name='displayblog'),
     path('patient/', views.patient, name='patient'),
-    path('superuser-login/', superuser_login, name='superuser_login'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login(request):
@@ -24,21 +21,3 @@ def displayblog(request):
 
 def patient(request):
     return render(request,'dashboard/patient.html')
-
-@user_passes_test(lambda u: u.is_superuser)
-def superuser_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard')  # Redirect to the dashboard or any other page
-        else:
-            # Handle authentication failure
-            return render(request, 'your_login_template.html', {'error_message': 'Invalid credentials'})
-
-    return render(request, 'your_login_template.html')
-
-
