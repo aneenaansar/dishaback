ef7283bd7960c702976dbe609fa3d4fd9b869188
Ajina Santhosh <139851101+ajinasanthosh1997@users.noreply.github.com>
2023-12-04 10:59:17 +0530

    updated login

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
+# from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -13,4 +14,5 @@ urlpatterns = [
     path('appoinments/',views.appoinments, name='appoinments'),
     path('displayblog/',views.displayblog, name='displayblog'),
     path('patient/', views.patient, name='patient'),
+    # path('accounts/', include('django.contrib.auth.urls')),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login  # Rename login to avoid conflicts
 
-# Create your views here.
 def login(request):
-    return render(request,'dashboard/login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)  # Use the renamed function
+            # Redirect to the admin dashboard or any other page
+            return redirect('dashboard:index')  # Replace 'dashboard:index' with your actual dashboard URL
+        else:
+            # Handle invalid login credentials
+            return render(request, 'dashboard/login.html', {'error': 'Invalid username or password'})
+    return render(request, 'dashboard/login.html')
 
 def index(request):
     return render(request,'dashboard/index.html')
 
 def addblog(request):
-    return render(request,'dashboard/addblog')
+    return render(request,'dashboard/addblog.html')
 
 def detail(request):
     return render(request,'dashboard/detail.html')
