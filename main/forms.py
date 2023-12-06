adf807a5aaf6b3f21b27c1910457dfb00a376d28
aneena ansar <131254828+aneenaansar@users.noreply.github.com>
2023-12-06 14:54:04 +0530

    updated

--- main/forms.py
+++ main/forms.py
@@ -1,16 +1,22 @@
 from django import forms
 from .models import Appointment
 
+TIME = (
+    ('Morning' , 'Morning'),
+    ('Evening' , 'Evening'),
+)
+
 
 
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
-        fields = ['name', 'email', 'contact', 'purpose', 'date']
+        fields = ['name', 'email', 'contact', 'purpose', 'date', 'time']
 
     name = forms.CharField(widget=forms.TextInput(attrs={'class' : 'border border-gray-800 rounded p-2 w-full'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'border border-gray-800 rounded p-2 w-full'}))
     contact = forms.CharField(widget=forms.TextInput(attrs={'class': 'border border-gray-800 rounded p-2 w-full'}))
     purpose = forms.CharField(widget=forms.TextInput(attrs={'class': 'border border-gray-800 rounded p-8 w-full'}))
     date = forms.DateField(widget=forms.TextInput(attrs={'class': 'border border-gray-800 rounded p-2 w-full','type': 'date'}))
+    time = forms.CharField(widget=forms.Select(choices=TIME,attrs={'class': 'border border-gray-800 rounded p-2 w-full'}))
 
--- main/models.py
+++ main/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+APPROVAL = (
+    ('Approved','Approved'),
+    ('Pending', 'Pending'),
+    ('Rejected','Rejected'),
+)
+
+
+
+STATUS = (
+    ('Scheduled','Scheduled'),
+    ('Pending', 'Pending'),
+    ('Canceled','Canceled'),
+)
+
+
+TIME = (
+    ('Morning' , 'Morning'),
+    ('Evening','Evening'),
+)
+
 # Create your models here.
 class Appointment(models.Model):
     name = models.CharField(max_length=100)
@@ -8,6 +28,8 @@ class Appointment(models.Model):
     contact = models.IntegerField()
     purpose = models.TextField(max_length=300)
     date = models.DateField()
+    status = models.CharField(max_length=50 , choices=STATUS , default='Pending' , blank=True,null=True)
+    time = models.CharField(max_length=50 , choices=TIME , blank=True,null=True)
 
 
     def __str__(self):
