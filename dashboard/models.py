2b9227881a006c0340d96d307018f7f78d8d3fe3
aneena ansar <131254828+aneenaansar@users.noreply.github.com>
2023-12-08 22:53:47 +0530

    update

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,7 +5,7 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, null=True, blank=True)
     date_of_birth = models.DateField(null=True)
-    age = models.IntegerField(max_length=150)
+    age = models.IntegerField(null=True)
     patient_id = models.CharField(max_length=20)
     gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
     address = models.TextField()
